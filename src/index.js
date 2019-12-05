e7ce4408f53ac2eea962c591abfb4821210f20c2
mccalluc <mccalluc@users.noreply.github.com>
2019-12-05 13:18:23 -0500

    Copy full example; missing styles

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
 import React, {Component} from 'react'
 
 import {
-    SearchkitManager, SearchkitProvider, SearchBox, Hits
+    SearchkitManager, SearchkitProvider, SearchBox,
+    HierarchicalMenuFilter, RefinementListFilter, LayoutResults,
+    ActionBar, ActionBarRow, HitsStats, SelectedFilters,
+    ResetFilters, MovieHitsGridItem, NoHits,
+    Hits, Layout, TopBar, LayoutBody, SideBar
 } from 'searchkit';
 
 
-const searchkit = new SearchkitManager("/");
+const searchkit = new SearchkitManager("http://demo.searchkit.co/api/movies/");
 
 export default class extends Component {
   render() {
     return (
       <SearchkitProvider searchkit={searchkit}>
-          <div>
-              <SearchBox/>
-              <Hits/>
-          </div>
+        <Layout>
+          <TopBar>
+            <SearchBox
+              autofocus={true}
+              searchOnChange={true}
+              prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+          </TopBar>
+          <LayoutBody>
+            <SideBar>
+              <HierarchicalMenuFilter
+                fields={["type.raw", "genres.raw"]}
+                title="Categories"
+                id="categories"/>
+              <RefinementListFilter
+                id="actors"
+                title="Actors"
+                field="actors.raw"
+                operator="AND"
+                size={10}/>
+            </SideBar>
+            <LayoutResults>
+              <ActionBar>
+
+                <ActionBarRow>
+                  <HitsStats/>
+                </ActionBarRow>
+
+                <ActionBarRow>
+                  <SelectedFilters/>
+                  <ResetFilters/>
+                </ActionBarRow>
+
+              </ActionBar>
+              <Hits mod="sk-hits-grid" hitsPerPage={10} itemComponent={MovieHitsGridItem}
+                sourceFilter={["title", "poster", "imdbId"]}/>
+              <NoHits/>
+            </LayoutResults>
+          </LayoutBody>
+        </Layout>
       </SearchkitProvider>
     );
   }
