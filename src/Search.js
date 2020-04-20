b8b1a2ba3b2baeeba91aabd3db6298b1180f7e9a
mccalluc <mccalluc@users.noreply.github.com>
2020-04-19 22:03:46 -0400

    ... and hide selectedFilter too

--- src/Search.js
+++ src/Search.js
@@ -54,23 +54,6 @@ function makeTableComponent(fields, detailsUrlPrefix, idField) {
   };
 }
 
-function SelectedFilter(props) {
-  // Copy and paste from
-  // http://docs.searchkit.co/v0.8.3/docs/components/navigation/selected-filters.html
-  // plus typo corrections.
-  return (
-    <div className={props.bemBlocks.option()
-      .mix(props.bemBlocks.container("item"))
-      .mix(`selected-filter--${props.filterId}`)}>
-      <div className={props.bemBlocks.option("name")}>{props.labelKey}!: {props.labelValue}</div>
-      <div className={props.bemBlocks.option("remove-action")} onClick={props.removeFilter}>x</div>
-    </div>
-  );
-}
-
-function MaskedSelectedFilters(props) {
-  return <SelectedFilters itemComponent={SelectedFilter} />
-}
 
 export default function (props) {
   const {
@@ -81,6 +64,28 @@ export default function (props) {
   } = props;
   const resultFieldsPlusId = [...resultFields, idField];
   const searchkit = new SearchkitManager(apiUrl, { httpHeaders, searchUrlPath });
+
+  function MaskedSelectedFilters(props) {
+    const SelectedFilter = (props) => {
+      const style = hiddenFilterIds.indexOf(props.filterId) === -1
+        ? {} : {display: 'None'};
+      // Copy and paste from
+      // http://docs.searchkit.co/v0.8.3/docs/components/navigation/selected-filters.html
+      // plus typo corrections and wrapping div.
+      return (
+        <div
+          style={style}
+          className={props.bemBlocks.option()
+            .mix(props.bemBlocks.container("item"))
+            .mix(`selected-filter--${props.filterId}`)}
+        >
+          <div className={props.bemBlocks.option("name")}>{props.labelKey}: {props.labelValue}</div>
+          <div className={props.bemBlocks.option("remove-action")} onClick={props.removeFilter}>x</div>
+        </div>
+      );
+    }
+    return <SelectedFilters itemComponent={SelectedFilter} />
+  }
 
   const filterElements = filters.map((def) => {
     const Filter = filterTypes[def.type];
